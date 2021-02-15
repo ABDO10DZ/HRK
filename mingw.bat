cb5618837b992c05d0b63a9a96d3e10657c2aec8
ABDO10 <33957096+ABDO10DZ@users.noreply.github.com>
2021-02-14 16:25:43 -0800

    Add files via upload

---
+++ mingw.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set /P MPATH=Enter MingW PATH:
+
+echo Compiler :  %MPATH%
+
+echo Compiling By Command Line :  %MPATH% main.cpp -static-libgcc -static-libstdc++ -Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive -o hrk.exe
+
+%MPATH% main.cpp -static-libgcc -static-libstdc++ -Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive -o hrk.exe
+
+echo Done , check ur current Directory if has new binaries file , that return to the tool 
+
+echo 
+echo run "hrk.exe" to see help menu , if not found please check ur "Compiler if match with this builder (for MingW) or compiler settings (Version,Place Holder)"
+echo if everyhting good and didn't build Please Contact Owner for the shit "https://t.me/ABDO10_DZ \ abdo10_dz@pm.me"
+
+   
