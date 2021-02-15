050634db9cc018437dbb8069228904c06c49357a
ABDO10 <33957096+ABDO10DZ@users.noreply.github.com>
2021-02-14 17:02:48 -0800

    Update mingw.bat

--- mingw.bat
+++ mingw.bat
@@ -13,5 +13,5 @@ echo Done , check ur current Directory if has new binaries file , that return to
 echo 
 echo run "hrk.exe" to see help menu , if not found please check ur "Compiler if match with this builder (for MingW) or compiler settings (Version,Place Holder)"
 echo if everyhting good and didn't build Please Contact Owner for the shit "https://t.me/ABDO10_DZ \ abdo10_dz@pm.me"
-
+PAUSE
    
